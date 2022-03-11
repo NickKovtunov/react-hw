5e7e84f2188a11fa87877f472268746d56937ddc
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2022-03-11 12:51:18 +0500

    И еще раз почистил

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { useEffect, useState } from "react";
+import {connect} from 'react-redux';
+
+function App(props: any) {
+
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
